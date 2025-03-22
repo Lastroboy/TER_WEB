b86ac654ce7e3ff968c165b819239752ca5d1562
Romain-Cariou <romain.cariou029@gmail.com>
2025-03-22 19:29:52 +0100

    Add files via upload

---
+++ Visualisation/CamembertGratuitéFetesEtManif.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv("Graphes/fetes-et-manifestations-nettoyé.csv")  
+colonne_interet = "TarifEntree"  
+
+data = df[colonne_interet].value_counts().reset_index()
+data.columns = ["Catégorie", "Nombre"]
+
+fig = px.pie(data,
+             names = "Catégorie",
+             values = "Nombre",
+             # title = "Gratuité des évènements",
+             color_discrete_sequence = px.colors.sequential.Blues[4:]
+             )
+
+# fig.show()
+
+fig.write_html("Graphes\GraphesTerminés\CamembertGratuitéFetesEtManif.html")
+
+# Je me suis aidé de "https://plotly.com/python/pie-charts/"
---
+++ Visualisation/CarteToutesLesDonnées.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import plotly.graph_objects as go
+
+df_activites = pd.read_csv("Graphes/GraphesTerminés/activités-sportives-culturelles_nettoyé.csv")
+df_festivals = pd.read_csv("Graphes/GraphesTerminés/festivals-de-cinema-en-bretagne-nettoyé.csv")
+df_fetes_et_manifs = pd.read_csv("Graphes/GraphesTerminés/fetes-et-manifestations-nettoyé.csv")
+
+carte = go.Figure()
+
+carte.add_trace(go.Scattermapbox(
+    lat = df_activites["latitude"], 
+    lon = df_activites["longitude"],
+    mode = "markers",
+    marker = dict(size = 10, color = "yellow"),
+    name = "Activités sportives et culturelles"
+))
+
+carte.add_trace(go.Scattermapbox(
+    lat = df_festivals["latitude"], 
+    lon = df_festivals["longitude"],
+    mode = "markers",
+    marker = dict(size = 10, color = "blue"),
+    name = "Festivals de cinéma"
+))
+
+carte.add_trace(go.Scattermapbox(
+    lat = df_fetes_et_manifs["Latitude"], 
+    lon = df_fetes_et_manifs["Longitude"],
+    mode = "markers",
+    marker = dict(size = 10, color = "purple"),
+    name = "Fetes et manifestations"
+))
+
+carte.update_layout(
+    mapbox = dict(
+        style = "open-street-map",
+        zoom = 7,
+        center = dict(lat = df_activites["latitude"].mean(), lon = df_activites["longitude"].mean())
+    ),
+    margin = dict(l = 0, r = 0, t = 0, b = 0),
+    legend = dict(x = 0, y = 1)
+)
+
+# carte.show()
+
+carte.write_html("Graphes\GraphesTerminés\CartesToutesLesDonnées.html")
+
+# Je me suis aidé de "https://plotly.com/python/maps/"
