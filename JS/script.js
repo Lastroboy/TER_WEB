8235f24e14a1a7a1f98467b88be829b0b221d65f
Noa Delaporte <noa.delaporte@gmail.com>
2025-03-23 20:12:26 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,6 @@
 const openButton = document.getElementById('open-sidebar-button')
 const navbar = document.getElementById('navbar')
 
-const media = window.matchMedia("(width < 900px)")
-
-media.addEventListener('change', (e) => updateNavbar(e))
-
-function updateNavbar(e){
-  const isMobile = e.matches
-  console.log(isMobile)
-  if(isMobile){
-    navbar.setAttribute('inert', '')
-  }
-  else{
-    // desktop device
-    navbar.removeAttribute('inert')
-  }
-}
-
 function openSidebar(){
   navbar.classList.add('show')
   openButton.setAttribute('aria-expanded', 'true')
@@ -28,13 +12,3 @@ function closeSidebar(){
   openButton.setAttribute('aria-expanded', 'false')
   navbar.setAttribute('inert', '')
 }
-
-// For Bookmark Links
-// const navLinks = document.querySelectorAll('nav a')
-// navLinks.forEach(link => {
-//   link.addEventListener('click', () => {
-//     closeSidebar()
-//   })
-// })
-
-updateNavbar(media)
