a40a11a0f1d75399a60c5ee6287e813cb570c587
Noa Delaporte <noa.delaporte@gmail.com>
2025-04-15 14:15:15 +0200

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+const openButton = document.getElementById('open-sidebar-button')
+const navbar = document.getElementById('navbar')
+
+function openSidebar(){
+  navbar.classList.add('show')
+  openButton.setAttribute('aria-expanded', 'true')
+  navbar.removeAttribute('inert')
+}
+
+function closeSidebar(){
+  navbar.classList.remove('show')
+  openButton.setAttribute('aria-expanded', 'false')
+  navbar.setAttribute('inert', '')
+}
